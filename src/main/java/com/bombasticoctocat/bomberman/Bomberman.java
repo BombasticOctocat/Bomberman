f343398e3827b8f10cad27117f1878ce37269a29
Marek Rusinowski <marekrusinowski@gmail.com>
2014-05-03 21:40:12 +0200

    Added resource directories and created very simple window layout

--- src/main/java/com/bombasticoctocat/bomberman/Bomberman.java
+++ src/main/java/com/bombasticoctocat/bomberman/Bomberman.java
@@ -1,7 +1,21 @@
 package com.bombasticoctocat.bomberman;
 
-public class Bomberman {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Bomberman extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setScene(new Scene(root, 640, 480));
+        primaryStage.setTitle("Bomberman");
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Bomberman");
+        launch(args);
     }
 }
