5221dfbf994b256564baeb38f44a4cbd07546c7f
Marek Rusinowski <marekrusinowski@gmail.com>
2014-05-04 18:52:21 +0200

    Added simple logging example to Bomberman.start()

--- src/main/java/com/bombasticoctocat/bomberman/Bomberman.java
+++ src/main/java/com/bombasticoctocat/bomberman/Bomberman.java
@@ -6,9 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Bomberman extends Application {
+    private static final Logger log = LoggerFactory.getLogger(Bomberman.class);
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        log.info("Started application");
+
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.setTitle("Bomberman");
