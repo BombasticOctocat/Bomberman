cb7fcd6034ec5434e918727cce1321db670f3951
Marek Rusinowski <marekrusinowski@gmail.com>
2014-05-04 22:57:10 +0200

    Implemended empty handlers for menu actions and exit handler with confirmation dialog

--- src/main/java/com/bombasticoctocat/bomberman/MainController.java
+++ src/main/java/com/bombasticoctocat/bomberman/MainController.java
@@ -1,14 +1,40 @@
 package com.bombasticoctocat.bomberman;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
+import org.controlsfx.control.action.Action;
+import org.controlsfx.dialog.Dialog;
+import org.controlsfx.dialog.Dialogs;
 
 public class MainController {
     @FXML private Pane windowPane;
 
     @FXML
-    private void handleMenuAction(ActionEvent event) {
-        System.out.println("You clicked me!");
+    private void handleNewClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void handleExitClick(ActionEvent actionEvent) {
+        Action response = Dialogs.create()
+            .owner(null)
+            .masthead(null)
+            .nativeTitleBar()
+            .title("Are you sure?")
+            .message("Are you sure you want to exit application?")
+            .showConfirm();
+
+        if (response == Dialog.Actions.YES) {
+            Platform.exit();
+        }
+    }
+
+    @FXML
+    private void handleSettingsClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void handleAboutClick(ActionEvent actionEvent) {
     }
 }
