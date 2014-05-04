883d6bf8116bf3dac695c4da37234d9026687531
Marek Rusinowski <marekrusinowski@gmail.com>
2014-05-04 23:36:11 +0200

    Added Logger to MainController and simple info messages about menu clicks

--- src/main/java/com/bombasticoctocat/bomberman/MainController.java
+++ src/main/java/com/bombasticoctocat/bomberman/MainController.java
@@ -4,19 +4,26 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
+
 import org.controlsfx.control.action.Action;
 import org.controlsfx.dialog.Dialog;
 import org.controlsfx.dialog.Dialogs;
 
+import org.slf4j.Logger;
+
 public class MainController {
     @FXML private Pane windowPane;
+    @InjectLog Logger log;
 
     @FXML
     private void handleNewClick(ActionEvent actionEvent) {
+        log.info("Clicked 'New'");
     }
 
     @FXML
     private void handleExitClick(ActionEvent actionEvent) {
+        log.info("Clicked 'Exit'");
+
         Action response = Dialogs.create()
             .owner(null)
             .masthead(null)
@@ -32,9 +39,11 @@ public class MainController {
 
     @FXML
     private void handleSettingsClick(ActionEvent actionEvent) {
+        log.info("Clicked 'Settings'");
     }
 
     @FXML
     private void handleAboutClick(ActionEvent actionEvent) {
+        log.info("Clicked 'About'");
     }
 }
