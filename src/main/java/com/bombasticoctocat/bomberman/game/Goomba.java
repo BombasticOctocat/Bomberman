787142bdd0214689779cca47d6ea6cec96f78482
Marek Rusinowski <marekrusinowski@gmail.com>
2014-05-26 21:23:42 +0200

    Increased goombas speed

--- src/main/java/com/bombasticoctocat/bomberman/game/Goomba.java
+++ src/main/java/com/bombasticoctocat/bomberman/game/Goomba.java
@@ -9,9 +9,9 @@ public class Goomba extends Particle {
     private static final int TRACK_WIDTH = 2;
 
     public enum Type {
-        LEVEL0(0.16, 0.5, 0.1, false),
-        LEVEL1(0.21, 0.5, 0.1, false),
-        LEVEL2(0.24, 0.2, 0.01, false),
+        LEVEL0(0.17, 0.5, 0.1, false),
+        LEVEL1(0.22, 0.5, 0.1, false),
+        LEVEL2(0.25, 0.2, 0.01, false),
         LEVEL3(0.33, 0.5, 0.1, false),
         LEVEL4(0.18, 0.6, 0.1, true),
         LEVEL5(0.24, 0.5, 0.1, true),
